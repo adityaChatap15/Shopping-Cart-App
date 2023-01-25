55ba0152e4457805efe97e6cb9e6a47cbdb2e2e0
Aditya Chatap <adityachatap123@gmail.com>
2023-01-25 22:33:26 +0530

    created the components and finsihed the header part .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import { BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/cart' exact>
+          <Cart />
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
